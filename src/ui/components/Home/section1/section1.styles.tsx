456cdc159bc34a0dd51652144e61f996e5f7de92
João Carlos Borges Chagas <joao.borges@wkm.com.br>
2021-06-15 10:19:55 -0300

    edição section1

--- src/ui/components/Home/section1/section1.styles.tsx
+++ src/ui/components/Home/section1/section1.styles.tsx
@@ -1,12 +1,73 @@
 import { experimentalStyled as styled } from '@material-ui/core/styles';
+import { Button, Container } from '@material-ui/core'
 
 export const BodySection = styled('div')`
     height: 755px;
     background-color: ${({ theme }) => theme.palette.primary.dark};
     position: absolute;
+    width: 100%;
+`;
+
+export const ContainerSction = styled(Container)`
     color: white;
+    padding-top: 55px;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    padding: 50px;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+`;
+
+export const Apresentacao = styled('div')`
+    font-family: 'Merriweather';
+    width: 50%;
+
+    h1 {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 46px;
+    line-height: 58px;
+    }
+
+    a {
+        color: white;
+    }
+
+    p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18.0119px;
+        line-height: 150%;
+    }
+
+    h2 {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 20.5851px;
+        line-height: 26px;
+    }
+
+`;
+
+
+export const Cadastros = styled(Container)`
+    flex-direction: row;
+    padding-top: 55px;
+`;
+
+export const ButtonCadastro = styled(Button)`
+    background-color: ${({ theme }) => theme.palette.background.paper};
+    color: black;
+    font-family: Open Sans;
+    font-style: normal;
+    font-weight: 600;
+    width: 222px;
+    box-shadow: 0px 3.43084px 12.008px rgba(0, 0, 0, 0.2);
+    border-radius: 6.86169px;
+    margin-right: 17px;
+    margin-top: 20px ;
+
+    &:hover{
+        background-color: #bebebe ;
+    }
 `;
--- src/ui/components/Home/section1/section1.tsx
+++ src/ui/components/Home/section1/section1.tsx
@@ -1,32 +1,50 @@
 import React from 'react'
-import { BodySection } from './section1.styles'
-import { Container } from '@material-ui/core'
+import {
+    BodySection,
+    Apresentacao,
+    ContainerSction,
+    Cadastros,
+    ButtonCadastro
+} from './section1.styles'
+import { Button } from '@material-ui/core'
 
 function section1() {
     return (
         <BodySection>
 
-            <div>
+            <ContainerSction>
+                <Apresentacao>
 
-                <h1> Conheça a Global ORG </h1>
+                    <h1> Conheça a Global ORG </h1>
 
-                <p>
-                    A GLOBAL ORG  faz parte do Conselho do Povo Brasileiro (CPB), organização que promove cooperação internacional e conecta projetos sociais com empresas, voluntários e pessoas que precisam de ajuda pelo mundo.
-                    Saiba mais
-                </p>
+                    <p>
+                        A GLOBAL ORG  faz parte do Conselho do Povo Brasileiro (CPB), organização que promove cooperação internacional e conecta projetos sociais com empresas, voluntários e pessoas que precisam de ajuda pelo mundo.
+                    </p>
 
-                <h2>
-                    Se interessou?
-                </h2>
+                    <a href="/"> Saiba mais </a>
 
-                <p>
-                    Cadastre‌ ‌a‌ ‌sua‌ ‌ONG‌ ‌na‌ ‌nossa‌ ‌plataforma‌ ‌e‌ ‌alcance‌ ‌voluntários‌ ‌do‌ ‌mundo‌ ‌todo.‌ ‌A‌ ‌Global‌ ORG‌ ‌também‌ ‌oferece‌ ‌uma‌ ‌série‌ ‌de‌ ‌ferramentas‌ ‌de‌ ‌gerenciamento‌ ‌para‌ ‌a‌ ‌sua‌ ‌ONG!‌
-                </p>
-            </div>
+                    <h2>
+                        Se interessou?
+                    </h2>
 
-            <div>
-                <img src={"https://images.vexels.com/media/users/3/157971/isolated/preview/393140c13ded6abdd322098d2d02a6d7-earth-planet-illustration-by-vexels.png"} alt="" width="200px" />
-            </div>
+                    <p>
+                        Cadastre‌ ‌a‌ ‌sua‌ ‌ONG‌ ‌na‌ ‌nossa‌ ‌plataforma‌ ‌e‌ ‌alcance‌ ‌voluntários‌ ‌do‌ ‌mundo‌ ‌todo.‌ ‌A‌ ‌Global‌ ORG‌ ‌também‌ ‌oferece‌ ‌uma‌ ‌série‌ ‌de‌ ‌ferramentas‌ ‌de‌ ‌gerenciamento‌ ‌para‌ ‌a‌ ‌sua‌ ‌ONG!‌
+                    </p>
+
+                    <ButtonCadastro variant="contained">Cadastre sua ONG</ButtonCadastro>
+
+                    <ButtonCadastro variant="contained">Seja um voluntário</ButtonCadastro>
+                </Apresentacao>
+
+                <div>
+                    <img src={"https://images.vexels.com/media/users/3/157971/isolated/preview/393140c13ded6abdd322098d2d02a6d7-earth-planet-illustration-by-vexels.png"} alt="" width="259px" />
+                </div>
+
+            </ContainerSction>
+
+
+
+
 
         </BodySection>
     )
