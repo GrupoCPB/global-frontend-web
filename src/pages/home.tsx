a2f195a3517b658238765b8e2183f6195fc3011e
João Carlos Borges Chagas <joao.borges@wkm.com.br>
2021-06-16 14:36:20 -0300

    fav icons globall ORG

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react'
 import Section1 from '../ui/components/Home/Section1/Section1';
 import Section2 from '../ui/components/Home/Section2/Section2';
@@ -5,9 +6,15 @@ import Section3 from '../ui/components/Home/Section3/Section3';
 import Section4 from '../ui/components/Home/Section4/Section4';
 import Section5 from '../ui/components/Home/Section5/Section5';
 
+
 function home() {
     return (
         <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <title>Global Org</title>
+                <meta name="description" content="" />
+            </Head>
             <Section1 />
             <Section2 />
             <Section3 />
