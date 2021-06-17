2cea57d88e0891aa66df4daa58d63bcdd0cce17b
João Carlos Borges Chagas <joao.borges@wkm.com.br>
2021-06-16 21:40:27 -0300

    inciando com PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,22 @@ function MyApp({ Component, pageProps }) {
         <title>Globall Org</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#11709e" />
       </Head>
       <ThemeProvider theme={theme}>
         <Header />
