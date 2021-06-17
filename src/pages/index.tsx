beefdcf6becc6aca7a11cc66b776a5bd295c4f01
João Carlos Borges Chagas <joao.borges@wkm.com.br>
2021-06-16 23:13:50 -0300

    test full sreen PWA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ export default function Home() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#11709e" />
+        <meta name="msapplication-TileImage" content="/assets/images/icons/icon-144x144.png" />
+        <meta name="msapplication-TileColor" content="#11709e" />
         <link rel="apple-touch-startup-image" href="public/android-icon-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="public/android-icon-750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
         <link rel="apple-touch-startup-image" href="public/android-icon-1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
