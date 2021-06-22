db51c29e06a205d5637247dc2b5f3447ef51c78a
João Carlos Borges Chagas <joao.borges@wkm.com.br>
2021-06-22 15:27:05 -0300

    LINKS REDES SOCIAIS, LINK HOME LOGO

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Toolbar } from '@material-ui/core';
 import {
   HeaderAppBar, HeaderLogo, SecondaryAppBar, InputBusca, SocialMedias,
 } from './Header.styles';
 
 function Header() {
+
   return (
     <>
       <HeaderAppBar position="relative">
 
-        <HeaderLogo src="/img/logo.png" alt="Globall org" />
+        <a href="/home">
+          <HeaderLogo src="/img/logo.png" alt="Global org" />
+        </a>
 
         <InputBusca placeholder="Busque vagas de voluntariado ou ONGs" />
 
         <SocialMedias>
-          <a href="#"><img src="/icons/YouTube.png" alt="" /></a>
-          <a href="#"><img src="/icons/LinkedIn.png" alt="" /></a>
-          <a href="#"><img src="/icons/IG.png" alt="" /></a>
-          <a href="#"><img src="/icons/WPP.png" alt="" /></a>
-          <a href="#"><img src="/icons/FB.png" alt="" /></a>
-          <a href="#"><img src="/icons/Twitter.png" alt="" /></a>
+          <a href="https://www.youtube.com/channel/UC5lXqh1Nl2E3RPTX0E9JxCA" target="_blank"><img src="/icons/YouTube.png" alt="global org youtube" /></a>
+          <a href="https://www.linkedin.com/company/global-org/" target="_blank"><img src="/icons/LinkedIn.png" alt="global org Linkedin" /></a>
+          <a href="https://www.instagram.com/globalorg_/" target="_blank"><img src="/icons/IG.png" alt="global org Instagram" /></a>
+          <a href="" target="_blank"><img src="/icons/WPP.png" alt="global org whatsApp" /></a>
+          <a href="https://www.facebook.com/globalll.org" target="_blank"><img src="/icons/FB.png" alt="global org FaceBook" /></a>
+          <a href="#" target="_blank"><img src="/icons/Twitter.png" alt="global org Twitter" /></a>
 
         </SocialMedias>
       </HeaderAppBar>
