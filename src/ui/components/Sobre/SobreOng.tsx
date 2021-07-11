f00b7aebe3175e0fa9a377cd3a7a93857bbf11b3
Amicuchi <andersonamicuchi@gmail.com>
2021-07-11 12:39:38 -0300

    Alteração na identação para resolução de problema.

--- src/ui/components/Sobre/SobreOng.tsx
+++ src/ui/components/Sobre/SobreOng.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import {
-    SobreOngBody,
-    SobreOngBtn,
-    SobreOngBtn2,
-    SobreOngContainer,
-    SobreOngContent,
-    SobreOngImgContent,
-    SobreOngImgContentImg,
-    SobreOngSegundoP,
+  SobreOngBody,
+  SobreOngBtn,
+  SobreOngBtn2,
+  SobreOngContainer,
+  SobreOngContent,
+  SobreOngImgContent,
+  SobreOngImgContentImg,
+  SobreOngSegundoP,
 
 } from "./SobreOng.styles"
 
@@ -17,31 +17,31 @@ import {
 
 export default function SobreOng() {
     return(
-        <SobreOngBody>
-            <SobreOngContainer>
-                <SobreOngContent>
-                    <h2>Quem somos</h2>
-                    <p>
-                    A GLOBAL ORG faz parte do Conselho do Povo Brasileiro (CPB), organização que promove cooperação internacional, conectando projetos sociais com empresas, voluntários e pessoas que precisam de ajuda pelo mundo.
-                    </p>
+      <SobreOngBody>
+        <SobreOngContainer>
+            <SobreOngContent>
+                <h2>Quem somos</h2>
+                <p>
+                A GLOBAL ORG faz parte do Conselho do Povo Brasileiro (CPB), organização que promove cooperação internacional, conectando projetos sociais com empresas, voluntários e pessoas que precisam de ajuda pelo mundo.
+                </p>
 
-                    <a href="#">Saiba mais</a>
+                <a href="#">Saiba mais</a>
 
-                    <h3>Se interessou?</h3>
-                    <SobreOngSegundoP>
-                    Então venha fazer a diferença! Você pode se candidatar nas vagas já disponíveis aqui ou oferecer diversos tipos de ajuda, clicando em Ajudar a comunidade.
-                    </SobreOngSegundoP>
+                <h3>Se interessou?</h3>
+                <SobreOngSegundoP>
+                Então venha fazer a diferença! Você pode se candidatar nas vagas já disponíveis aqui ou oferecer diversos tipos de ajuda, clicando em Ajudar a comunidade.
+                </SobreOngSegundoP>
 
-                    <SobreOngBtn>
-                        <Button variant="contained" color="secondary">Cadastre sua ONG</Button>
-                        <SobreOngBtn2 variant="contained" color="secondary">Ajudar a comunidade</SobreOngBtn2>
-                    </SobreOngBtn>
-                </SobreOngContent>
-                
-                <SobreOngImgContent>
-                    <SobreOngImgContentImg src="/img/home/globo.svg" />
-                </SobreOngImgContent>
-            </SobreOngContainer>
-        </SobreOngBody>
+                <SobreOngBtn>
+                <Button variant="contained" color="secondary">Cadastre sua ONG</Button>
+                <SobreOngBtn2 variant="contained" color="secondary">Ajudar a comunidade</SobreOngBtn2>
+                </SobreOngBtn>
+            </SobreOngContent>
+            
+            <SobreOngImgContent>
+                <SobreOngImgContentImg src="/img/home/globo.svg" />
+            </SobreOngImgContent>
+        </SobreOngContainer>
+    </SobreOngBody>
     )
 }
