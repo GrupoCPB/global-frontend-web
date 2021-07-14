28472f19ece294d0635a540bb31d5102a67d4999
Joao <bruno.machado98@hotmail.com>
2021-07-14 16:35:16 -0300

    ok

--- src/ui/components/Home/Section2/Section2.styles.tsx
+++ src/ui/components/Home/Section2/Section2.styles.tsx
@@ -1,43 +1,40 @@
 import { experimentalStyled as styled } from '@material-ui/core/styles';
-import { Button, Container } from '@material-ui/core'
+import { Button, Container } from '@material-ui/core';
 
 export const BodySection = styled('div')`
-    background-color:  ${({ theme }) => theme.palette.background.paper};
-    width: 100%;
+  background-color: ${({ theme }) => theme.palette.background.paper};
+  width: 100%;
 `;
-
 
 export const ContainerSction = styled(Container)`
-    padding-top: 55px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    flex-wrap: wrap;
-    padding-bottom: 55px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+    top: -105px;
+    align-content: center;
+    position: relative;
 `;
 
-
 export const Cadastros = styled(Container)`
-    flex-direction: row;
-    padding-top: 55px;
+  flex-direction: row;
+  padding-top: 55px;
 `;
 
 export const ButtonCadastro = styled(Button)`
-    
-    background-color:  ${({ theme }) => theme.palette.primary.dark};
-    font-family: Open Sans;
-    font-style: normal;
-    font-weight: 600;
-    width: 222px;
-    box-shadow: 0px 3.43084px 12.008px rgba(0, 0, 0, 0.2);
-    border-radius: 6.86169px;
-    margin-right: 17px;
-    margin-top: 20px ;
-
-     ${({ theme }) => theme.breakpoints.down('md')}{
-        width: 100%;
-     }
+  background-color: ${({ theme }) => theme.palette.primary.dark};
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  width: 222px;
+  box-shadow: 0px 3.43084px 12.008px rgba(0, 0, 0, 0.2);
+  border-radius: 6.86169px;
+  margin-right: 17px;
+  margin-top: 20px;
+  ${({ theme }) => theme.breakpoints.down('md')} {
+    width: 100%;
+  }
 `;
 
 export const GroupCard = styled(Container)`
