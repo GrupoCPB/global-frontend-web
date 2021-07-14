4a601d73e725096a33aa42b6f0d1d90c385658a4
Amicuchi <andersonamicuchi@gmail.com>
2021-07-14 20:21:23 -0300

    Atualização do código da página Sobre

--- src/ui/components/Sobre/SobreOng.styles.tsx
+++ src/ui/components/Sobre/SobreOng.styles.tsx
@@ -1,85 +1,40 @@
 import { experimentalStyled as styled } from '@material-ui/core/styles';
-import { Button } from "@material-ui/core"
+import { Button, Container } from '@material-ui/core';
 
-export const resetPagina = styled('div')`
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+export const BodySection = styled('div')`
+    background-color: ${({ theme }) => theme.palette.primary.dark};
+    color: ${({ theme }) => theme.palette.grey[50]};
+    width: 100%;
+    padding-bottom: 170px;
     font-family: 'Merriweather', serif;
 `;
 
-export const SobreOngBody = styled('body')`
-    background-color: ${({ theme }) => theme.palette.primary.dark};
-    color: ${({ theme }) => theme.palette.grey[50]};
-    display: flex;
-`;
-
-export const SobreOngContainer = styled('div')`
-    padding: 0px 100px 50px;
+export const ContainerSection = styled(Container)`
     display: flex;
     flex-wrap: wrap;
+    flex-direction: row;
     justify-content: space-around;
+    align-items: center;
+    padding-bottom: 4rem;
+    padding-top: 2rem;
 `;
 
-export const SobreOngContent = styled('div')`
-    width: 50%;
-
-    h2{
-        font-weight: 300;
-        font-size: 46px;
-        line-height: 2em;
-    }
-
-    h3{
-        font-weight: 300;
-        font-size: 20px;
-        line-height: 2em;
-        margin-top: 30px;
-    }
-
-    p{
-        font-weight: 300;
-        font-size: 16px;
-        line-height: 1.6em;
-        text-align: left;
-    }
-
-    a{
-        font-weight: 300;
-        font-size: 14px;
-        color: ${({ theme }) => theme.palette.grey[50]};
-        margin-bottom: 40px;
-        text-align: right;
-    }
-`;
-    
-export const SobreOngSegundoP = styled('p')`
-    font-weight: 300;
-    font-size: 16px;
-    line-height: 1.6em;
-    text-align: left;
-    margin-bottom: 40px;
-`;
-
-export const SobreOngBtn = styled('div')`
+export const ButtonCadastro = styled(Button)`
+    background-color: ${({ theme }) => theme.palette.background.paper};
+    color: black;
+    font-family: Open Sans;
+    font-weight: 600;
+    width: 222px;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
     border-radius: 7px;
-    border: none;
-`;
+    margin-right: 17px;
+    margin-top: 20px ;
 
-export const SobreOngBtn2 = styled(Button)`
-    margin-left: 15px;
-`;
+    &:hover{
+        background-color:  ${({ theme }) => theme.palette.grey[400]};;
+    }
 
-export const SobreOngImgContent = styled('div')`
-    margin-top: auto;
-    margin-bottom: auto;
-    
     ${({ theme }) => theme.breakpoints.down('md')}{
         width: 100%;
-    }
-`;
-
-export const SobreOngImgContentImg = styled('img')`
-    width: 488px;
-    height: 327px;
+     }
 `;
