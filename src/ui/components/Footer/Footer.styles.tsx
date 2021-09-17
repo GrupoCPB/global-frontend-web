62ad0d1ae2eb720dcb6477b1b5ad10ef05098f60
bruno <jp.bruno.machado@gmail.com>
2021-09-17 20:37:22 -0300

    .

--- src/ui/components/Footer/Footer.styles.tsx
+++ src/ui/components/Footer/Footer.styles.tsx
@@ -148,6 +148,7 @@ export const LowerBar = styled("div")`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 15px 0 20px 0;
 
     span {
         font-size: 16px;
